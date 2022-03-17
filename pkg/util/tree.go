c07e6491f173272ad530d460523f8e4a25b848ab
chenxi <bbxycx.18@163.com>
2022-03-17 11:03:59 +0800

    无限极分类

--- pkg/util/tree.go
+++ pkg/util/tree.go
@@ -18,8 +18,14 @@ func GenerateTree(list interface{}, pid uint64) []TreeList {
 	svs := sv.Slice(0, sv.Len())
 	for i := 0; i < svs.Len(); i++ {
 		e := svs.Index(i)
-		currentPid := e.FieldByName("ParentId").Uint()
-		currentId := e.FieldByName("Id").Uint()
+		var currentPid, currentId uint64
+		if e.Kind() == reflect.Struct {
+			currentPid = e.FieldByName("ParentId").Uint()
+			currentId = e.FieldByName("Id").Uint()
+		} else {
+			currentPid = e.Elem().FieldByName("ParentId").Uint()
+			currentId = e.Elem().FieldByName("Id").Uint()
+		}
 		if currentPid == pid {
 			child := GenerateTree(list, currentId)
 			node := TreeList{
