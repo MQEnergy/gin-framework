9d465f5ba0fb31c9cdad5a92884f3cca682c9437
chenxi <bbxycx.18@163.com>
2022-03-23 16:26:02 +0800

    【requestid中间件优化】解决requestid中间件导致的post请求参数获取不到的问题

--- middleware/requestid_auth.go
+++ middleware/requestid_auth.go
@@ -2,9 +2,11 @@ package middleware
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"mqenergy-go/global"
+	"time"
 )
 
 type CustomResponseWriter struct {
@@ -25,21 +27,32 @@ func (w CustomResponseWriter) WriteString(s string) (int, error) {
 // RequestIdAuth requestId中间件
 func RequestIdAuth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		body, _ := ctx.GetRawData()
-		writer := CustomResponseWriter{body: bytes.NewBufferString(""), ResponseWriter: ctx.Writer}
+		writer := CustomResponseWriter{
+			body:           bytes.NewBufferString(""),
+			ResponseWriter: ctx.Writer,
+		}
 		ctx.Writer = writer
+		// 开始时间
+		reqStartTime := time.Now().UnixMilli()
+		// 处理请求
 		ctx.Next()
+		if ctx.Request.Method != "GET" {
+			ctx.Request.ParseForm()
+		}
 		var responseData string
+		// 结束时间
+		reqEndTime := time.Now().UnixMilli()
 		fields := logrus.Fields{
-			"_body":     string(body),
-			"_host":     ctx.Request.Host,
-			"_method":   ctx.Request.Method,
-			"_clientIp": ctx.ClientIP(),
-			"_uri":      ctx.Request.RequestURI,
+			"req_body":     ctx.Request.PostForm.Encode(),
+			"req_host":     ctx.Request.Host,
+			"req_method":   ctx.Request.Method,
+			"req_clientIp": ctx.ClientIP(),
+			"req_uri":      ctx.Request.RequestURI,
+			"res_time":     fmt.Sprintf("%vms", reqEndTime-reqStartTime), // 响应时间
 		}
 		if ctx.Writer.Status() != 200 {
 			responseData = writer.body.String()
-			fields["_header"] = ctx.Request.Header
+			fields["req_header"] = ctx.Request.Header
 			// 记录request日志
 			global.Logger.WithFields(fields).Warn(responseData)
 		} else {
