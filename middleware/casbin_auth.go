64fe1981aff5cab1b413c9ad9bfc3a16297bab5a
chenxi <bbxycx.18@163.com>
2022-05-22 17:00:40 +0800

    【框架优化】优化中间件casbin

--- middleware/casbin_auth.go
+++ middleware/casbin_auth.go
@@ -2,20 +2,30 @@ package middleware
 
 import (
 	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/util"
 	gormadapter "github.com/casbin/gorm-adapter/v3"
 	"github.com/gin-gonic/gin"
 	"mqenergy-go/global"
 	"mqenergy-go/global/app"
 	"mqenergy-go/pkg/response"
+	"os"
 	"strconv"
+	"strings"
 )
 
 // CasbinAuth 用户权限验证
 func CasbinAuth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		adapter, _ := gormadapter.NewAdapterByDB(global.DB)
-		e, _ := casbin.NewEnforcer("rbac_model.conf", adapter)
-		err := e.LoadPolicy()
+		dir, err := os.Getwd()
+		if err != nil {
+			ctx.Abort()
+			return
+		}
+		e, _ := casbin.NewEnforcer(dir+"/config/rbac_model.conf", adapter)
+		e.AddFunction("ParamsMatch", ParamsMatchFunc)
+		_ = e.LoadPolicy()
+
 		//	获取当前请求的url
 		obj := ctx.Request.URL.RequestURI()
 		act := ctx.Request.Method
@@ -23,6 +33,7 @@ func CasbinAuth() gin.HandlerFunc {
 		if err != nil {
 			response.UnauthorizedException(ctx, "权限异常")
 			ctx.Abort()
+			return
 		}
 		var flag = false
 		for _, sub := range user.RoleIds {
@@ -30,13 +41,27 @@ func CasbinAuth() gin.HandlerFunc {
 			subStr := strconv.FormatUint(sub, 10)
 			if ok, _ := e.Enforce(subStr, obj, act); ok {
 				flag = true
+				break
 			}
 		}
-		if flag {
-			ctx.Next()
-		} else {
+		if !flag {
 			response.UnauthorizedException(ctx, "该用户无此权限")
 			ctx.Abort()
+			return
 		}
+		ctx.Next()
 	}
+}
+
+// ParamsMatchFunc 自定义规则函数
+func ParamsMatchFunc(args ...interface{}) (interface{}, error) {
+	name1 := args[0].(string)
+	name2 := args[1].(string)
+	return ParamsMatch(name1, name2), nil
+}
+
+// ParamsMatch 自定义规则函数
+func ParamsMatch(fullNameKey1 string, key2 string) bool {
+	key1 := strings.Split(fullNameKey1, "?")[0]
+	return util.KeyMatch2(key1, key2)
 }
--- middleware/login_auth.go
+++ middleware/login_auth.go
@@ -5,28 +5,30 @@ import (
 	"mqenergy-go/config"
 	"mqenergy-go/pkg/auth"
 	"mqenergy-go/pkg/response"
-	"net/http"
 	"strings"
 )
 
 // LoginAuth 登录中间件
-func LoginAuth(ctx *gin.Context) {
-	token := ctx.GetHeader(config.Conf.Server.TokenKey)
-	if token == "" {
-		ctx.JSON(http.StatusUnauthorized, response.CodeMap[response.UnAuthed])
-		ctx.Abort()
-		return
+func LoginAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader(config.Conf.Server.TokenKey)
+		if token == "" {
+			response.UnauthorizedException(ctx, "")
+			ctx.Abort()
+			return
+		}
+		b := strings.Contains(token, "Bearer")
+		if !b {
+			response.UnauthorizedException(ctx, "")
+			ctx.Abort()
+			return
+		}
+		token = strings.TrimSpace(strings.TrimLeft(token, "Bearer"))
+		if _, err := auth.ParseJwtToken(token, config.Conf.Server.JwtSecret); err != nil {
+			response.UnauthorizedException(ctx, err.Error())
+			ctx.Abort()
+			return
+		}
+		ctx.Next()
 	}
-	b := strings.Contains(token, "Bearer")
-	if !b {
-		ctx.JSON(http.StatusUnauthorized, response.CodeMap[response.UnAuthed])
-		ctx.Abort()
-		return
-	}
-	token = strings.TrimSpace(strings.TrimLeft(token, "Bearer"))
-	if _, err := auth.ParseJwtToken(token, config.Conf.Server.JwtSecret); err != nil {
-		ctx.Abort()
-		return
-	}
-	ctx.Next()
 }
