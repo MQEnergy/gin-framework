61bb13cc69dac7aa1991e841bbfd95d6f765c364
chenxi <bbxycx.18@163.com>
2023-07-01 13:59:47 +0800

    增加admin账号的生成

--- cmd/admin.go
+++ cmd/admin.go
@@ -14,6 +14,7 @@ import (
 var (
 	account  string
 	password string
+	roleId   string
 )
 
 // AccountCmd 管理者账号创建命令
@@ -46,6 +47,14 @@ func AccountCmd() *cli.Command {
 				Destination: &password,
 				Required:    true,
 			},
+			&cli.StringFlag{
+				Name:        "role",
+				Aliases:     []string{"r"},
+				Value:       "",
+				Usage:       "请输入角色ID 如：1,2,3.. 或连续的1,2,3",
+				Destination: &roleId,
+				Required:    true,
+			},
 		},
 		Action: func(ctx *cli.Context) error {
 			bootstrap.BootService(bootstrap.MysqlService)
@@ -56,27 +65,41 @@ func AccountCmd() *cli.Command {
 
 // generateAdmin 生成admin信息工具
 func generateAdmin() error {
-	salt := util.GenerateUuid(32)
-	admin := &models.GinAdmin{
-		Uuid:         util.GenerateBaseSnowId(0, nil),
+	var (
+		adminInfo models.GinAdmin
+		salt      = util.GenerateUuid(32)
+		uuid      = util.GenerateBaseSnowId(0, nil)
+		pass      = util.GeneratePasswordHash(password, salt)
+		timeNow   = uint64(time.Now().Unix())
+	)
+	if err := global.DB.First(&adminInfo, "account = ?", account).Error; err == nil {
+		fmt.Println(fmt.Sprintf("\x1b[31m%s\x1b[0m", "account: "+account+" is already existed"))
+		return err
+	}
+	localIp, err := util.GetLocalIp()
+	if err != nil {
+		return err
+	}
+	adminInfo = models.GinAdmin{
+		Uuid:         uuid,
 		Account:      account,
-		Password:     util.GeneratePasswordHash(password, salt),
-		Phone:        "12345678901",
+		Password:     pass,
+		Phone:        "",
 		Avatar:       "",
 		Salt:         salt,
 		RealName:     account,
-		RegisterTime: uint64(time.Now().Unix()),
-		RegisterIp:   "127.0.0.1",
-		LoginTime:    uint64(time.Now().Unix()),
-		LoginIp:      "127.0.0.1",
-		RoleIds:      "1",
+		RegisterTime: timeNow,
+		RegisterIp:   localIp,
+		LoginTime:    0,
+		LoginIp:      "",
+		RoleIds:      roleId,
 		Status:       1,
-		CreatedAt:    uint64(time.Now().Unix()),
-		UpdatedAt:    uint64(time.Now().Unix()),
+		CreatedAt:    timeNow,
+		UpdatedAt:    timeNow,
 	}
-	if err := global.DB.Create(admin).Error; err != nil {
+	if err := global.DB.Create(&adminInfo).Error; err != nil {
 		return err
 	}
-	fmt.Println(fmt.Sprintf("\u001B[34m%s\u001B[0m", "账号："+account+"; 密码："+password+" 生成成功"))
+	fmt.Println(fmt.Sprintf("\u001B[34m%s\u001B[0m", fmt.Sprintf("账号：%s 密码：%s 角色ID：%s 生成成功", account, password, roleId)))
 	return nil
 }
--- pkg/util/node.go
+++ pkg/util/node.go
@@ -41,3 +41,20 @@ func GetLocalIpToInt() (int, error) {
 	}
 	return 0, errors.New("can not find the client ip address")
 }
+
+// GetLocalIp 获取本机ip
+func GetLocalIp() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", errors.New("can not find the client ip address")
+}
