6da91f6d655d64d828b438aec3ed6b502424b42b
chenxi <bbxycx.18@163.com>
2023-07-05 15:04:59 +0800

    优化定时器

--- pkg/util/ticker.go
+++ pkg/util/ticker.go
@@ -9,6 +9,7 @@ type Fn func() error
 type MyTicker struct {
 	MyTick *time.Ticker
 	Runner Fn
+	Done   chan bool // 添加一个Done通道
 }
 
 // NewTicker 实例化定时器
@@ -16,15 +17,25 @@ func NewTicker(interval int, f Fn) *MyTicker {
 	return &MyTicker{
 		MyTick: time.NewTicker(time.Duration(interval) * time.Second),
 		Runner: f,
+		Done:   make(chan bool), // 初始化Done通道
 	}
 }
 
 // Start 启动定时器需要执行的任务
 func (t *MyTicker) Start() {
-	for {
-		select {
-		case <-t.MyTick.C:
-			t.Runner()
+	go func() {
+		for {
+			select {
+			case <-t.MyTick.C:
+				t.Runner()
+			case <-t.Done: // 添加Done通道的处理逻辑
+				t.MyTick.Stop()
+			}
 		}
-	}
+	}()
+}
+
+// Stop 停止定时器
+func (t *MyTicker) Stop() {
+	t.Done <- true
 }
